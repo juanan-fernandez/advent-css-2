eea3f069fee4cff7a24b319bbd2896b0b952febc
juanan fernandez <juanan.fernandez@gmail.com>
2022-12-16 18:41:55 +0100

    initial

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,6 +1,50 @@
-export const menu = [{
-   id:1,
-   name: "dd",
-   img:""
-   price:""
-}];
+export const menu = [
+	{
+		id: 1,
+		name: 'Bacon and eggs with grilled tomatoes and advocado. ',
+		img: '../assets/img_plates/plate__bacon-eggs.png',
+		price: 4.5,
+	},
+	{
+		id: 2,
+		name: 'Chicken salad with species and chili peeper.',
+		img: '../assets/img_plates/plate__chicken-salad.png',
+		price: 5.5,
+	},
+	{
+		id: 3,
+		name: 'Fish & Chips',
+		img: '../assets/img_plates/plate__fish-sticks-fries.png',
+		price: 6.5,
+	},
+	{
+		id: 4,
+		name: 'French Fries with ketchup',
+		img: '../assets/img_plates/plate__french-fries.png',
+		price: 4.5,
+	},
+	{
+		id: 5,
+		name: 'Ravioli with mushrooms and shrimps',
+		img: '../assets/img_plates/plate__ravioli.png',
+		price: 8.5,
+	},
+	{
+		id: 6,
+		name: 'Salmon with vegetables and boiled potatos',
+		img: '../assets/img_plates/plate__salmon-vegetables.png',
+		price: '8.50',
+	},
+	{
+		id: 7,
+		name: 'Spaguetti with Meat Sauce',
+		img: '../assets/img_plates/plate__spaghetti-meat-sauce.png',
+		price: 6.5,
+	},
+	{
+		id: 8,
+		name: 'Tortellini with albahaca and olive oil',
+		img: '../assets/img_plates/plate__bacon-eggs.png',
+		price: 6.5,
+	},
+];
