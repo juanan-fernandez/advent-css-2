eff5d8ffa2bb79c38c6dcf7e06d59217b7b6bb5c
juanan fernandez <juanan.fernandez@gmail.com>
2022-12-21 14:22:25 +0100

    CartItem css

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,13 +1,31 @@
 import './CartItem.css';
-
+import minorLogo from '../../../assets/chevron.svg';
+import plusLogo from '../../../assets/chevron_right.svg';
 const CartItem = ({ id, name, price, units, img }) => {
 	return (
 		<div className='cart-item'>
 			<div className='cart-item__img'>
 				<img src={img} />
-				<span>2</span>
+				<span>{units}</span>
 			</div>
-			<p>{name}</p>
+			<div className='cart-item__info'>
+				<div>
+					<p>{name}</p>
+					<p>{price}</p>
+				</div>
+				<div className='cart-item__info--buttons'>
+					<div>
+						<button>
+							<img src={minorLogo} alt='One less' />
+						</button>
+						<span>2</span>
+						<button>
+							<img className='right' src={plusLogo} alt='One more' />
+						</button>
+					</div>
+					<h1>$22.00</h1>
+				</div>
+			</div>
 		</div>
 	);
 };
