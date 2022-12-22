8bc2a1e1afc0d00ce8a814f96fbc2f9bc3ba7f37
juanan fernandez <juanan.fernandez@gmail.com>
2022-12-22 21:35:49 +0100

    button already in cart

--- src/components/Menu/MenuItem/MenuItem.jsx
+++ src/components/Menu/MenuItem/MenuItem.jsx
@@ -1,15 +1,30 @@
 import './MenuItem.css';
-import { useContext } from 'react';
+import checkSvg from '../../../assets/check.svg';
+import { format2Digits } from '../../../utils/numbers';
+import { useContext, useState } from 'react';
 import CartContext from '../../../store/cart-context';
 
 const MenuItem = ({ id, name, price, img, color }) => {
 	const cartCtx = useContext(CartContext);
-
-	const format2Digits = myNumber => (Math.round(myNumber * 100) / 100).toFixed(2);
+	const [isInCart, setIsInCart] = useState(false);
+	//const format2Digits = myNumber => (Math.round(myNumber * 100) / 100).toFixed(2);
 
 	const buttonClickHandler = () => {
 		cartCtx.addToCart({ id, name, price, img });
+		setIsInCart(true);
 	};
+
+	const btnAddToCart = !isInCart ? (
+		<button className='item__button' onClick={buttonClickHandler}>
+			Add To Cart
+		</button>
+	) : (
+		<button className='item__button--in-cart'>
+			<img src={checkSvg} alt='Already in cart' />
+			&nbsp;&nbsp;&nbsp;&nbsp; In Cart
+		</button>
+	);
+
 	return (
 		<div className='item'>
 			<img src={img}></img>
@@ -19,9 +34,7 @@ const MenuItem = ({ id, name, price, img, color }) => {
 					<h1>${format2Digits(price)}</h1>
 				</div>
 			</div>
-			<button className='item__button' onClick={buttonClickHandler}>
-				Add To Cart
-			</button>
+			{btnAddToCart}
 		</div>
 	);
 };
